f3c85552df6871f343bd653ccc59400b927860f8
marcosalexper <marcosalexper@hotmail.com>
2024-10-11 16:10:27 -0300

    feat: Deleção de usuário com DELETE

--- src/main/java/com/marcosalexper/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosalexper/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void deleteById(String id) {
+		findBydId(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
