f908819f95381cfe39ad43af976faa6d919b9bcd
marcosalexper <marcosalexper@hotmail.com>
2024-10-11 15:33:39 -0300

    feat: Obtendo um usuario por id

--- src/main/java/com/marcosalexper/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosalexper/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.marcosalexper.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcosalexper.workshopmongo.domain.User;
 import com.marcosalexper.workshopmongo.repository.UserRepository;
+import com.marcosalexper.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findBydId(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
 }
