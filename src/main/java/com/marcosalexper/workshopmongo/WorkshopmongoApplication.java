6f8a1f029628d0ed8ec0256807091437868481a8
marcosalexper <marcosalexper@hotmail.com>
2024-10-11 12:53:14 -0300

    feat: Conectando ao MongoDB

--- src/main/java/com/marcosalexper/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/marcosalexper/workshopmongo/WorkshopmongoApplication.java
@@ -2,10 +2,8 @@ package com.marcosalexper.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.marcosalexper.workshopmongo.repository")
 public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
