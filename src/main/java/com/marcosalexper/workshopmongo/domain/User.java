80fc8e29b8346d923bfbc0d126b64a6424465d43
marcosalexper <marcosalexper@hotmail.com>
2024-10-15 17:06:16 -0300

    feat: Referenciando os posts do usuario

--- src/main/java/com/marcosalexper/workshopmongo/domain/User.java
+++ src/main/java/com/marcosalexper/workshopmongo/domain/User.java
@@ -1,22 +1,28 @@
 package com.marcosalexper.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
+@Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
 	
-	public User() {		
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
+	public User() {
 	}
 
 	public User(String id, String name, String email) {
@@ -50,6 +56,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -66,7 +80,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
